9aabdd4def40708a7eee0efdd07a8d069ddd1156
asifkhan <khanasifxyz@gmail.com>
2023-08-31 17:33:57 +0530

    Revert "Bug Fix"
    This reverts commit 6c7f51011a07381a7a119ee31b0d14c5fbfd0030.

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,25 @@ import Courses from './pages/courses/mern/Mern';
 import Footer from './pages/footer/Footer'
 import Hire from './pages/hire/components/Hire'
 import HireFromUs from './pages/hireFromUs/HireFromUs';
-// import Skilled from './pages/hireFromUs/components/Skilled'
+import Skilled from './pages/hireFromUs/components/Skilled'
 import OtherCourse from './pages/othersCourse/OtherCourse';
 import Mentorship from './pages/mentorship/Mentorship';
-// import RoadMap from './pages/mentorship/components/roadmap/RoadMap';
+import RoadMap from './pages/mentorship/components/roadmap/RoadMap';
 import Navbar from './pages/Navbar/Navbar';
 
 const App = () => {
   return (
     <div>
-      <Navbar></Navbar>
+      {/* <Navbar></Navbar> */}
 
-      <Mentorship></Mentorship>
+      {/* <Mentorship></Mentorship> */}
 
-      <Courses></Courses>
-      <MentorshipProgram></MentorshipProgram>
-      <OtherCourse></OtherCourse>
-      <Footer></Footer>
-      <Hire></Hire>
-      <HireFromUs></HireFromUs>
+      {/* <Courses></Courses> */}
+      {/* <MentorshipProgram></MentorshipProgram> */}
+      {/* <OtherCourse></OtherCourse> */}
+      {/* <Footer></Footer> */}
+      {/* <Hire></Hire> */}
+      {/* <HireFromUs></HireFromUs> */}
     </div>
   );
 };
